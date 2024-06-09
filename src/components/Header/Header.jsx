df1df9b0c0d77aa3d818afadf981ed4f29db74e1
Mardon <mardonfayzullaev2006@gmail.com>
2024-06-09 10:31:13 +0300

    modified:   src/App.js 	renamed:    src/components/Button1/Button.css -> src/components/ButtonFldr/Button.css 	renamed:    src/components/Button1/Button.jsx -> src/components/ButtonFldr/Button.jsx 	modified:   src/components/Header/Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,7 @@
 import React from "react";
-import Button from "../Button1/Button"
-import {useTelegram} from "../../hooks/useTelegram";
+import Button from "../ButtonFldr/Button"
+import './Header.css'; //на удаление
+import { useTelegram } from "../../hooks/useTelegram";
 
 const Header = () => {
     const {user, onClose} = useTelegram();
